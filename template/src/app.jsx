f518346a6e91a8d4bd88f2fbef504c5a7f893dc7
Fábio Batista <fbatista@gmail.com>
2015-04-10 16:15:07 +0100

    Fixed gitignore

--- template/src/app.jsx
+++ template/src/app.jsx
@@ -6,5 +6,3 @@ import 'isomorphic-fetch';
 import React from 'react/addons';
 import Router from 'react-router';
 import StylingMixin from './utils/styling-mixin.jsx';
-import Scroller from './scroller-component.jsx';
-import sortBy from './sort';
